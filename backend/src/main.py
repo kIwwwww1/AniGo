2769f3d0e5853695b4c95c69ca1d8a36ea0fb4c6
kIwwwww1 <danil29070@gmail.com>
2025-12-26 10:21:28 +0300

    Запуст api

---
+++ backend/src/main.py
@@ -0,0 +1,22 @@
+import asyncio
+import uvicorn
+from loguru import logger
+from typing import Callable
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+# app.include_router(...)
+
+# @app.middleware('http')
+# async def middleware_main_app(request: Request, call_next: Callable):
+#     response = await call_next(request)
+#     logger.info('Сервис запущен')
+#     return response
+
+
+async def main():
+    pass
+
+if __name__ == '__main__':
+    uvicorn.run('backend.src.main:app', reload=True, host='0.0.0.0', port=8000)
