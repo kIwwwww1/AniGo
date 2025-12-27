961b234b47a3441e4f2c6e6d3a2fd014dbdc43dd
kIwwwww1 <danil29070@gmail.com>
2025-12-27 13:31:28 +0300

    Проверка данных

--- backend/src/services/users.py
+++ backend/src/services/users.py
@@ -5,35 +5,40 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.models.users import UserModel
 from src.schemas.user import CreateNewUser
 
-async def get_user_by_email(user_email: str, user_name: str, session: AsyncSession):
-    user = (await session.execute(select(UserModel).filter_by(email=user_email, username=user_name))).scalar_one_or_none()
-    return user
+async def get_user_by_name(name: str, session: AsyncSession):
+    user = (await session.execute(select(UserModel).filter_by(username=name))).scalar_one_or_none()
+    if user is None:
+        return user
+    raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Ник занят'
+            )
 
 
-# async def get_user_by_name(user_name: str, session: AsyncSession):
-#     user = (await session.execute(select(UserModel).filter_by(username=user_name))).scalar_one_or_none()
-#     if user:
-#         raise HTTPException(
-#             status_code=status.HTTP_409_CONFLICT,
-#             detail='Пользователь с  сущ')
-#     return user
+async def get_user_by_email(email: str, session: AsyncSession):
+    user = (await session.execute(select(UserModel).filter_by(email=email))).scalar_one_or_none()
+    if user is None:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail='почта занята'
+        )
+
+
+async def user_exists(name: str, email: str, session: AsyncSession):
+    await get_user_by_name(name, session)
+    await get_user_by_email(email, session)
 
 
 async def add_user(new_user: CreateNewUser, session: AsyncSession):
-    # if (await get_user_by_email(new_user.email, session)) and (await get_user_by_name(new_user.username, session)):
-    user = await get_user_by_email(new_user.email, new_user.username, session)
-    if user:
-        raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail='Пользователь с ником сууууущ')
-    session.add(UserModel(
-        username=new_user.username,
-        email=new_user.email,
-        password_hash=new_user.password,
-    ))
-
-    await session.commit()
-    return 'Добавлен'
+    if await user_exists(new_user.username, new_user.email, session) is None:
+        session.add(UserModel(
+            username=new_user.username,
+            email=new_user.email,
+            password_hash=new_user.password,
+        ))
+        await session.commit()
+        return 'Добавлен'
 
 
 
