df819aef86cbcb3e091bb5a19e54ce33f44d9194
kIwwwww1 <danil29070@gmail.com>
2026-01-06 19:10:26 +0300

    s3

---
+++ backend/src/services/s3.py
@@ -0,0 +1,44 @@
+from aiobotocore.session import get_session
+from fastapi import UploadFile
+from contextlib import asynccontextmanager
+
+class S3Client:
+    def __init__(
+            self, access_key: str,
+            secret_key: str, 
+            endpoint_url: str,
+            bucket_name: str):
+        
+        self.config = {
+            'aws_access_key_id': access_key,
+            'aws_secret_access_key': secret_key,
+            'endpoint_url': endpoint_url,
+        }
+        self.bucket_name = bucket_name
+        self.session = get_session()
+
+
+    @asynccontextmanager
+    async def get_client(self):
+        async with self.session.create_client("s3", **self.config) as client:
+            yield client
+            
+    async def upload_file(self, user_photo: UploadFile, user_id: int):
+
+        object_name = 'photo/profile/user_{user_id}'
+        file_context = user_photo.read()
+
+        async with self.get_client() as client:
+            await client.put_object(
+                Bucket=self.bucket_name,
+                Key=object_name,
+                Body=file_context,
+            )
+            
+
+s3_client = S3Client(
+    access_key='287f46f37d90482980a2e453b185163f',
+    secret_key='a16f368a0146495dbc86c259d9ef0ebf',
+    endpoint_url='https://s3.storage.selcloud.ru',
+    bucket_name='anigo'
+)
